c9783d4620c28c283df9fa1a0a85cab6ab653989
Randy Tolentino <makertolentino@gmail.com>
2017-01-01 05:21:54 -0600

    adding starter eslinting

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './main.css';
 
 const year = new Date().getFullYear();
 
-console.log(`Happy New Year! It's ${year} yo!`);
+console.log(`Happy New Year! It's ${year} yo!`); // eslint-disable-line no-console
